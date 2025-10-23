3d3c7de428e4cfa538386ec4590c9ce45d89b008
Jannic Schneider <Jannschn22@gmail.com>
2025-10-23 15:12:58 +0000

    Commit

--- Code-Me/23.10.2025/bin_find.go
+++ Code-Me/23.10.2025/bin_find.go
@@ -1,23 +1,52 @@
 package searching
 
-// Liefert die Position x in der Liste l
+// Liefert die Position von x in der Liste l,
 // oder liefert -1, falls x nicht in l vorkommt.
-func BinFind(l []int, x int) int {
-	for {
+func BinFind1(l []int, x int) int {
+	links := 0
+	for len(l) > 0 {
 		mitte := len(l) / 2
-		//vergleiche x mit dem Element in der Mitte.
-		//wenn x == l[mitte], dann fertig
-		if x == l[mitte] {
+		// Vergleiche x mit dem Element in der Mitte.
+		// Wenn x == l[mitte], dann fertig.
+		if l[mitte] == x {
+			return mitte + links
+		}
+
+		// Wenn x < l[mitte], dann suche im linken Teil weiter.
+		if x < l[mitte] {
+			// Lasse nur den linken Teil der Liste übrig.
+			// Alles von 0 bis exklusive mitte.
+			//l = l[0:mitte]
+			l = l[:mitte]
+		} else {
+			// Wenn x > l[mitte], dann suche im rechten Teil weiter.
+			// Lasse nur den rechten Teil der Liste übrig.
+			//l = l[mitte+1 : len(l)]
+			l = l[mitte+1:]
+			links += mitte + 1
+		}
+	}
+
+	return -1
+}
+
+// Liefert die Position von x in der Liste l,
+// oder liefert -1, falls x nicht in l vorkommt.
+func BinFind2(l []int, x int) int {
+	links := 0       // Hier beginnt der interessante Teil der Liste.
+	rechts := len(l) // Hier endet der interessante Teil der Liste.
+	for rechts > links {
+		mitte := (rechts-links)/2 + links
+
+		if l[mitte] == x {
 			return mitte
 		}
-		//Wenn x < l[mitte], dann suche
-		//im Linken teil Weiter.
 		if x < l[mitte] {
-			l = l[0:mitte]
+			rechts = mitte
 		} else {
-			l = l[mitte+1:]
+			links = mitte + 1
 		}
-		//Wenn x > l[mitte], dann suche im Rechten teil Weiter.
-		return -1
 	}
+
+	return -1
 }
---
+++ Code-Me/23.10.2025/bin_find_rek.go
@@ -0,0 +1,27 @@
+package searching
+
+//Liefert die position x in der Liste l,
+//oder liefert -1 falls x nicht in l vorkommt.
+
+func BinFindRek(l []int, x int) int {
+	if len(l) == 0 {
+		return -1
+	}
+	mitte := len(l) / 2
+	if x == l[mitte] {
+		return mitte
+	}
+	if x < l[mitte] {
+		//Suche im Linken Teil weiter.
+		return BinFindRek(l[:mitte], x)
+	}
+	if x > l[mitte] {
+		//Suche im rechten Teil weiter.
+		result := BinFindRek(l[mitte+1:], x)
+		if result == -1 {
+			return -1 
+		}
+		return result + mitte +1
+	}
+	return -1 
+}
